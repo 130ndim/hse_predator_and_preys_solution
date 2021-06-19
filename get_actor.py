936f01bc8c66db7ff7223e005e2145a3bdf48c6a
Dmitry <130ndim@gmail.com>
2021-06-19 14:42:56 +0300

    rm unnecessary files

--- get_actor.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-
-torch.save(torch.load('./pred_ddpg.pt', map_location='cpu')['actor'], 'pred_ddpg_actor.pt')
-torch.save(torch.load('./prey_ddpg.pt', map_location='cpu')['actor'], 'prey_ddpg_actor.pt')
